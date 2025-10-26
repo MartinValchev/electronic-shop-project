91f979b45715a132fd350fd47eb14099892dec7c
mvalchev <martinvalchev1@gmail.com>
2025-10-26 23:27:53 +0200

    added eureka server and clients

--- OrderService/src/main/java/com/martin/proj/OrderService/OrderServiceApplication.java
+++ OrderService/src/main/java/com/martin/proj/OrderService/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.martin.proj.OrderService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
